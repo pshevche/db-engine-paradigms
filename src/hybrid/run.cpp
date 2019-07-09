2b46427cf4fa38528dd572bfb2500fd6b54a68fa
pshevche <pavel.shevchenko.95@gmail.com>
2019-07-09 11:30:43 +0200

    add q1 + q6 experiments

--- src/hybrid/run.cpp
+++ src/hybrid/run.cpp
@@ -59,7 +59,7 @@ int main(int argc, char* argv[]) {
    bool clearCaches = false;
    if (argc > 3) nrThreads = atoi(argv[3]);
 
-   std::unordered_set<std::string> q = {"1hv"};
+   std::unordered_set<std::string> q = {"1v", "1h", "1hv", "6v", "6h", "6hv"};
 
    if (auto v = std::getenv("vectorSize")) vectorSize = atoi(v);
    if (auto v = std::getenv("SIMDhash")) conf.useSimdHash = atoi(v);
