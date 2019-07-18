9b7dbec776585ce20fc8989ccfbbccd82d7bceb4
pshevche <pavel.shevchenko.95@gmail.com>
2019-07-18 16:07:43 +0200

    fix avx issues at remote

--- include/common/runtime/Hash.hpp
+++ include/common/runtime/Hash.hpp
@@ -2,7 +2,7 @@
 #include "common/Compat.hpp"
 #include "common/Util.hpp"
 #include "common/defs.hpp"
-#include "common/runtime/SIMD.hpp"
+// #include "common/runtime/SIMD.hpp"
 #include "common/runtime/Types.hpp"
 #include <x86intrin.h>
 
@@ -20,62 +20,62 @@ template <typename CHILD> class Hash {
  public:
    // inline uint64_t hashKey(const void* key, int len, uint64_t seed) const { }
 
-  using hash_t = defs::hash_t;
-  /// Integers
-  inline hash_t operator()(uint64_t x, hash_t seed) const {
-     return impl()->hashKey(x, seed);
-  }
-  inline hash_t operator()(int64_t x, hash_t seed) const {
-     return impl()->hashKey(x, seed);
-  }
-  inline hash_t operator()(uint32_t x, hash_t seed) const {
-     return impl()->hashKey(x, seed);
-  }
-  inline hash_t operator()(int32_t x, hash_t seed) const {
-     return impl()->hashKey(x, seed);
-  }
-  inline hash_t operator()(int8_t x, hash_t seed) const {
-     return impl()->hashKey(x, seed);
-  }
+   using hash_t = defs::hash_t;
+   /// Integers
+   inline hash_t operator()(uint64_t x, hash_t seed) const {
+      return impl()->hashKey(x, seed);
+   }
+   inline hash_t operator()(int64_t x, hash_t seed) const {
+      return impl()->hashKey(x, seed);
+   }
+   inline hash_t operator()(uint32_t x, hash_t seed) const {
+      return impl()->hashKey(x, seed);
+   }
+   inline hash_t operator()(int32_t x, hash_t seed) const {
+      return impl()->hashKey(x, seed);
+   }
+   inline hash_t operator()(int8_t x, hash_t seed) const {
+      return impl()->hashKey(x, seed);
+   }
 
-  /// Pointers
-  inline hash_t operator()(void* ptr, hash_t seed) const {
-     return impl()->hashKey(reinterpret_cast<uint64_t>(ptr), seed);
-  }
+   /// Pointers
+   inline hash_t operator()(void* ptr, hash_t seed) const {
+      return impl()->hashKey(reinterpret_cast<uint64_t>(ptr), seed);
+   }
 
-  inline hash_t operator()(types::Integer&& x, hash_t seed) const {
-     return impl()->hashKey(x.value, seed);
-  }
-  inline hash_t operator()(const types::Integer& x, hash_t seed) const {
-     return impl()->hashKey(x.value, seed);
-  }
+   inline hash_t operator()(types::Integer&& x, hash_t seed) const {
+      return impl()->hashKey(x.value, seed);
+   }
+   inline hash_t operator()(const types::Integer& x, hash_t seed) const {
+      return impl()->hashKey(x.value, seed);
+   }
 
-  inline hash_t operator()(types::Date&& x, hash_t seed) const {
-     return impl()->hashKey(x.value, seed);
-  }
-  inline hash_t operator()(const types::Date& x, hash_t seed) const {
-     return impl()->hashKey(x.value, seed);
-  }
+   inline hash_t operator()(types::Date&& x, hash_t seed) const {
+      return impl()->hashKey(x.value, seed);
+   }
+   inline hash_t operator()(const types::Date& x, hash_t seed) const {
+      return impl()->hashKey(x.value, seed);
+   }
 
-  template <unsigned len, unsigned precision>
-  inline hash_t operator()(types::Numeric<len, precision>&& x,
-                           hash_t seed) const {
-     return impl()->hashKey(x.value, seed);
-  }
-  template <unsigned len, unsigned precision>
-  inline hash_t operator()(const types::Numeric<len, precision>& x,
-                           hash_t seed) const {
-     return impl()->hashKey(x.value, seed);
-  }
+   template <unsigned len, unsigned precision>
+   inline hash_t operator()(types::Numeric<len, precision>&& x,
+                            hash_t seed) const {
+      return impl()->hashKey(x.value, seed);
+   }
+   template <unsigned len, unsigned precision>
+   inline hash_t operator()(const types::Numeric<len, precision>& x,
+                            hash_t seed) const {
+      return impl()->hashKey(x.value, seed);
+   }
 
-  template <unsigned l>
-  inline hash_t operator()(const types::Char<l>& x, hash_t seed) const;
-  template <unsigned l>
-  inline hash_t operator()(types::Char<l>&& x, hash_t seed) const;
+   template <unsigned l>
+   inline hash_t operator()(const types::Char<l>& x, hash_t seed) const;
+   template <unsigned l>
+   inline hash_t operator()(types::Char<l>&& x, hash_t seed) const;
 
-  template <unsigned l>
-  inline hash_t operator()(const types::Varchar<l>& x, hash_t seed) const {
-     return impl()->hashKey(&x.value, x.len, seed);
+   template <unsigned l>
+   inline hash_t operator()(const types::Varchar<l>& x, hash_t seed) const {
+      return impl()->hashKey(&x.value, x.len, seed);
    }
    template <unsigned l>
    inline hash_t operator()(types::Varchar<l>&& x, hash_t seed) const {
@@ -156,12 +156,11 @@ class StdHash {
 
 class MurMurHash : public Hash<MurMurHash> {
  public:
-
-// #ifdef __AVX512F__
-//   inline Vec8u operator()(Vec8u& k, Vec8u& seed) const {
-//     return ->hashKey(k, seed);
-//   }
-// #endif
+   // #ifdef __AVX512F__
+   //   inline Vec8u operator()(Vec8u& k, Vec8u& seed) const {
+   //     return ->hashKey(k, seed);
+   //   }
+   // #endif
    inline hash_t hashKey(uint64_t k) const { return hashKey(k, 0); }
    inline hash_t hashKey(uint64_t k, hash_t seed) const {
       // MurmurHash64A
@@ -207,13 +206,16 @@ class MurMurHash : public Hash<MurMurHash> {
       const unsigned char* data2 = (const unsigned char*)data;
 
       switch (len & 7) {
-      case 7: h ^= uint64_t(data2[6]) << 48;FALLTHROUGH
-      case 6: h ^= uint64_t(data2[5]) << 40;FALLTHROUGH
-      case 5: h ^= uint64_t(data2[4]) << 32;FALLTHROUGH
-      case 4: h ^= uint64_t(data2[3]) << 24;FALLTHROUGH
-      case 3: h ^= uint64_t(data2[2]) << 16;FALLTHROUGH
-      case 2: h ^= uint64_t(data2[1]) << 8;FALLTHROUGH
-      case 1: h ^= uint64_t(data2[0]); h *= m;FALLTHROUGH
+      case 7: h ^= uint64_t(data2[6]) << 48; FALLTHROUGH
+      case 6: h ^= uint64_t(data2[5]) << 40; FALLTHROUGH
+      case 5: h ^= uint64_t(data2[4]) << 32; FALLTHROUGH
+      case 4: h ^= uint64_t(data2[3]) << 24; FALLTHROUGH
+      case 3: h ^= uint64_t(data2[2]) << 16; FALLTHROUGH
+      case 2: h ^= uint64_t(data2[1]) << 8; FALLTHROUGH
+      case 1:
+         h ^= uint64_t(data2[0]);
+         h *= m;
+         FALLTHROUGH
       };
 
       h ^= h >> r;
@@ -223,227 +225,227 @@ class MurMurHash : public Hash<MurMurHash> {
       return h;
    }
 
-  //#ifdef __AVX512F__
-#ifdef __AVX512DQ__
-   inline Vec8u hashKey(Vec8u k, Vec8u seed) const {
-      // MurmurHash64A
-      const Vec8u m(0xc6a4a7935bd1e995);
-      const Vec8u r(47);
-      Vec8u h = seed ^ Vec8u(0x8445d61a4e774912) ^ (Vec8u(8) * m);
-      k = k * m;
-      k = k ^ (k >> r);
-      k = k * m;
-      h = h ^ k;
-      h = h * m;
-      h = h ^ (h >> r);
-      h = h * m;
-      h = h ^ (h >> r);
-      return h;
-   }
-#endif
-
+   //   //#ifdef __AVX512F__
+   // #ifdef __AVX512DQ__
+   //    inline Vec8u hashKey(Vec8u k, Vec8u seed) const {
+   //       // MurmurHash64A
+   //       const Vec8u m(0xc6a4a7935bd1e995);
+   //       const Vec8u r(47);
+   //       Vec8u h = seed ^ Vec8u(0x8445d61a4e774912) ^ (Vec8u(8) * m);
+   //       k = k * m;
+   //       k = k ^ (k >> r);
+   //       k = k * m;
+   //       h = h ^ k;
+   //       h = h * m;
+   //       h = h ^ (h >> r);
+   //       h = h * m;
+   //       h = h ^ (h >> r);
+   //       return h;
+   //    }
+   // #endif
 };
 
 EXTRAOPS(MurMurHash)
 
-
-inline uint32_t rotl32 ( uint32_t x, int8_t r ) {
-  return (x << r) | (x >> (32 - r));
+inline uint32_t rotl32(uint32_t x, int8_t r) {
+   return (x << r) | (x >> (32 - r));
 }
-inline uint64_t rotl64 ( uint64_t x, int8_t r ) {
-    return (x << r) | (x >> (64 - r));
-}
-
-#define	FORCE_INLINE inline __attribute__((always_inline))
-#define	ROTL32(x,y)	rotl32(x,y)
-#define ROTL64(x,y) rotl64(x,y)
-
-FORCE_INLINE uint32_t fmix32 ( uint32_t h ) {
-    h ^= h >> 16;
-    h *= 0x85ebca6b;
-    h ^= h >> 13;
-    h *= 0xc2b2ae35;
-    h ^= h >> 16;
-    return h;
-}
-#ifdef __AVX512F__
-FORCE_INLINE Vec16u fmix32 ( Vec16u h ) {
-    h = h ^ (h >> 16);
-    h = h * Vec16u(0x85ebca6b);
-    h = h ^ (h >> 13);
-    h = h * Vec16u(0xc2b2ae35);
-    h = h ^ (h >> 16);
-    return h;
-}
-#endif
-FORCE_INLINE uint32_t getblock32 ( const uint32_t * p, int i ) {
-    return p[i];
+inline uint64_t rotl64(uint64_t x, int8_t r) {
+   return (x << r) | (x >> (64 - r));
 }
 
-#define IS_INT_LE(bits, t) (sizeof(t)<=(bits/8) &&        \
-                          std::is_integral<t>::value)
+#define FORCE_INLINE inline __attribute__((always_inline))
+#define ROTL32(x, y) rotl32(x, y)
+#define ROTL64(x, y) rotl64(x, y)
 
-#define IS_INT_EQ(bits, t) (sizeof(t)==(bits/8) &&      \
-                            std::is_integral<t>::value)
+FORCE_INLINE uint32_t fmix32(uint32_t h) {
+   h ^= h >> 16;
+   h *= 0x85ebca6b;
+   h ^= h >> 13;
+   h *= 0xc2b2ae35;
+   h ^= h >> 16;
+   return h;
+}
+// #ifdef __AVX512F__
+// FORCE_INLINE Vec16u fmix32 ( Vec16u h ) {
+//     h = h ^ (h >> 16);
+//     h = h * Vec16u(0x85ebca6b);
+//     h = h ^ (h >> 13);
+//     h = h * Vec16u(0xc2b2ae35);
+//     h = h ^ (h >> 16);
+//     return h;
+// }
+// #endif
+FORCE_INLINE uint32_t getblock32(const uint32_t* p, int i) { return p[i]; }
+
+#define IS_INT_LE(bits, t)                                                     \
+   (sizeof(t) <= (bits / 8) && std::is_integral<t>::value)
+
+#define IS_INT_EQ(bits, t)                                                     \
+   (sizeof(t) == (bits / 8) && std::is_integral<t>::value)
 // 32-bit hashes
 class MurMurHash3 : public Hash<MurMurHash3> {
  public:
+   // #ifdef __AVX512F__
+   //   inline Vec8u operator()(Vec8u& k, Vec8u& seed) const {
+   //     return ->hashKey(k, seed);
+   //   }
+   // #endif
 
-// #ifdef __AVX512F__
-//   inline Vec8u operator()(Vec8u& k, Vec8u& seed) const {
-//     return ->hashKey(k, seed);
-//   }
-// #endif
+   template <class T>
+   inline auto hashKey(T k, hash_t seed) const ->
+       typename std::enable_if<IS_INT_LE(32, T), hash_t>::type {
+      uint32_t h1 = seed;
+      const uint32_t c1 = 0xcc9e2d51;
+      const uint32_t c2 = 0x1b873593;
 
-  template<class T> inline auto
-  hashKey(T k, hash_t seed) const -> typename std::enable_if<IS_INT_LE(32,T), hash_t>::type{
-     uint32_t h1 = seed;
-     const uint32_t c1 = 0xcc9e2d51;
-     const uint32_t c2 = 0x1b873593;
+      uint32_t k1 = k;
 
-     uint32_t k1 = k;
+      k1 *= c1;
+      k1 = ROTL32(k1, 15);
+      k1 *= c2;
 
-     k1 *= c1;
-     k1 = ROTL32(k1,15);
-     k1 *= c2;
+      h1 ^= k1;
+      h1 = ROTL32(h1, 13);
+      h1 = h1 * 5 + 0xe6546b64;
 
-     h1 ^= k1;
-     h1 = ROTL32(h1,13);
-     h1 = h1*5+0xe6546b64;
-
-     return fmix32(h1);
+      return fmix32(h1);
    }
    inline hash_t hashKey(uint64_t k) const { return hashKey(k, 0); }
 
-   template<class T> inline auto
-   hashKey(T k, hash_t seed) const -> typename std::enable_if<IS_INT_EQ(64,T), hash_t>::type{
-   // inline hash_t hashKey(uint64_t k, hash_t seed) const {
-     uint32_t h1 = seed;
-     const uint32_t c1 = 0xcc9e2d51;
-     const uint32_t c2 = 0x1b873593;
+   template <class T>
+   inline auto hashKey(T k, hash_t seed) const ->
+       typename std::enable_if<IS_INT_EQ(64, T), hash_t>::type {
+      // inline hash_t hashKey(uint64_t k, hash_t seed) const {
+      uint32_t h1 = seed;
+      const uint32_t c1 = 0xcc9e2d51;
+      const uint32_t c2 = 0x1b873593;
 
-     uint32_t k1 = k;
-     uint32_t k2 = k >> 32;
+      uint32_t k1 = k;
+      uint32_t k2 = k >> 32;
 
-     k1 *= c1;
-     k1 = ROTL32(k1,15);
-     k1 *= c2;
+      k1 *= c1;
+      k1 = ROTL32(k1, 15);
+      k1 *= c2;
 
-     h1 ^= k1;
-     h1 = ROTL32(h1,13);
-     h1 = h1*5+0xe6546b64;
+      h1 ^= k1;
+      h1 = ROTL32(h1, 13);
+      h1 = h1 * 5 + 0xe6546b64;
 
-     k2 *= c1;
-     k1 = ROTL32(k1,15);
-     k2 *= c2;
+      k2 *= c1;
+      k1 = ROTL32(k1, 15);
+      k2 *= c2;
 
-     h1 ^= k2;
-     h1 = ROTL32(h1,13);
-     h1 = h1*5+0xe6546b64;
+      h1 ^= k2;
+      h1 = ROTL32(h1, 13);
+      h1 = h1 * 5 + 0xe6546b64;
 
-     return fmix32(h1);
+      return fmix32(h1);
    }
 
    hash_t hashKey(const void* key, int len, hash_t seed) const {
-     // from: https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
-     const uint8_t * data = (const uint8_t*)key;
-     const int nblocks = len / 4;
+      // from:
+      // https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
+      const uint8_t* data = (const uint8_t*)key;
+      const int nblocks = len / 4;
 
-     uint32_t h1 = seed;
+      uint32_t h1 = seed;
 
-     const uint32_t c1 = 0xcc9e2d51;
-     const uint32_t c2 = 0x1b873593;
+      const uint32_t c1 = 0xcc9e2d51;
+      const uint32_t c2 = 0x1b873593;
 
-     //----------
-     // body
+      //----------
+      // body
 
-     const uint32_t * blocks = (const uint32_t *)(data + nblocks*4);
+      const uint32_t* blocks = (const uint32_t*)(data + nblocks * 4);
 
-     for(int i = -nblocks; i; i++)
-       {
-         uint32_t k1 = getblock32(blocks,i);
+      for (int i = -nblocks; i; i++) {
+         uint32_t k1 = getblock32(blocks, i);
 
          k1 *= c1;
-         k1 = ROTL32(k1,15);
+         k1 = ROTL32(k1, 15);
          k1 *= c2;
 
          h1 ^= k1;
-         h1 = ROTL32(h1,13);
-         h1 = h1*5+0xe6546b64;
-       }
+         h1 = ROTL32(h1, 13);
+         h1 = h1 * 5 + 0xe6546b64;
+      }
 
-     //----------
-     // tail
+      //----------
+      // tail
 
-     const uint8_t * tail = (const uint8_t*)(data + nblocks*4);
+      const uint8_t* tail = (const uint8_t*)(data + nblocks * 4);
 
-     uint32_t k1 = 0;
+      uint32_t k1 = 0;
 
-     switch(len & 3)
-       {
-       case 3: k1 ^= tail[2] << 16;
-       case 2: k1 ^= tail[1] << 8;
-       case 1: k1 ^= tail[0];
-         k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
-       };
+      switch (len & 3) {
+      case 3: k1 ^= tail[2] << 16;
+      case 2: k1 ^= tail[1] << 8;
+      case 1:
+         k1 ^= tail[0];
+         k1 *= c1;
+         k1 = ROTL32(k1, 15);
+         k1 *= c2;
+         h1 ^= k1;
+      };
 
-     //----------
-     // finalization
+      //----------
+      // finalization
 
-     h1 ^= len;
+      h1 ^= len;
 
-     h1 = fmix32(h1);
+      h1 = fmix32(h1);
 
-     return h1;
+      return h1;
    }
 
-#ifdef __AVX512F__
+   // #ifdef __AVX512F__
 
-  //#ifdef __AVX512DQ__
-   inline Vec16u hashKey(Vec16u k, Vec16u seed) const {
-     auto h1 = seed;
-     Vec16u c1(0xcc9e2d51);
-     Vec16u c2(0x1b873593);
+   //   //#ifdef __AVX512DQ__
+   //    inline Vec16u hashKey(Vec16u k, Vec16u seed) const {
+   //      auto h1 = seed;
+   //      Vec16u c1(0xcc9e2d51);
+   //      Vec16u c2(0x1b873593);
 
-     auto k1 = k;
+   //      auto k1 = k;
 
-     k1 = k1 * c1;
-     k1 = _mm512_rol_epi32(k1, 15);
-     k1 = k1 * c2;
+   //      k1 = k1 * c1;
+   //      k1 = _mm512_rol_epi32(k1, 15);
+   //      k1 = k1 * c2;
 
-     h1 = h1 ^ k1;
-     h1 = _mm512_rol_epi32(h1, 13);
+   //      h1 = h1 ^ k1;
+   //      h1 = _mm512_rol_epi32(h1, 13);
 
-     h1 = h1*Vec16u(5)+Vec16u(0xe6546b64);
+   //      h1 = h1*Vec16u(5)+Vec16u(0xe6546b64);
 
-     return fmix32(h1);
-   }
-#endif
-
+   //      return fmix32(h1);
+   //    }
+   // #endif
 };
-
 
 EXTRAOPS(MurMurHash3)
 
 class CRC32Hash : public Hash<CRC32Hash> {
 
  public:
-
    // template<class T> inline auto
-   // hashKey(T k, hash_t seed) const -> typename std::enable_if<IS_INT_LE(32,T), hash_t>::type{
+   // hashKey(T k, hash_t seed) const -> typename
+   // std::enable_if<IS_INT_LE(32,T), hash_t>::type{
    //   const uint64_t c1= 11400714819323198485ull;
    //   const uint64_t c2= 11500714819323198569ull;
    //   return (__builtin_bswap32(k) * c2) ^ (k * c1) ^ (ROTL64(seed, 15));
    // }
 
    // template<class T> inline auto
-   // hashKey(T k, hash_t seed) const -> typename std::enable_if<IS_INT_LE(32,T), hash_t>::type{
+   // hashKey(T k, hash_t seed) const -> typename
+   // std::enable_if<IS_INT_LE(32,T), hash_t>::type{
    //    return _mm_crc32_u64(seed, k);
    // }
 
-   template<class T> inline auto
-   hashKey(T k, hash_t seed) const -> typename std::enable_if<IS_INT_LE(64,T), hash_t>::type{
-   // inline hash_t hashKey(uint64_t k, uint64_t seed) const {
+   template <class T>
+   inline auto hashKey(T k, hash_t seed) const ->
+       typename std::enable_if<IS_INT_LE(64, T), hash_t>::type {
+      // inline hash_t hashKey(uint64_t k, uint64_t seed) const {
       uint64_t result1 = _mm_crc32_u64(seed, k);
       uint64_t result2 = _mm_crc32_u64(0x04c11db7, k);
       return ((result2 << 32) | result1) * 0x2545F4914F6CDD1Dull;
@@ -464,9 +466,9 @@ class CRC32Hash : public Hash<CRC32Hash> {
          len -= 4;
       }
       switch (len) {
-      case 3: s ^= ((uint64_t)data[2]) << 16;FALLTHROUGH
-      case 2: s ^= ((uint64_t)data[1]) << 8;FALLTHROUGH
-      case 1: s ^= data[0];FALLTHROUGH
+      case 3: s ^= ((uint64_t)data[2]) << 16; FALLTHROUGH
+      case 2: s ^= ((uint64_t)data[1]) << 8; FALLTHROUGH
+      case 1: s ^= data[0]; FALLTHROUGH
       }
       return s;
    }
